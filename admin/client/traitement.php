d15260486409a1ab490e15382c1062d4a33da87f
MelvilPE <127743721+MelvilPRO@users.noreply.github.com>
2024-01-22 15:28:13 +0100

    Update traitement.php

--- admin/client/traitement.php
+++ admin/client/traitement.php
@@ -1,10 +1,23 @@
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/connect.php";
+	$clientNom = "";
+	$clientPrenom = "";
+
+	if (isset($_POST["clientNom"]))
+	{
+		$clientNom = $_POST["clientNom"];
+	}
+		
+	if (isset($_POST["clientPrenom"]))
+	{
+		$clientPrenom = $_POST["clientPrenom"];
+	}
+	
 	$sql = "INSERT INTO tblclient (clientNom, clientPrenom)
 			VALUES (:nom, :prenom)";
 
 	$statement = $db->prepare($sql);
-	$statement->execute([":nom"=>$_POST["clientNom"],
-						 ":prenom"=>$_POST["clientPrenom"]]);
+	$statement->execute([":nom"=>$clientNom,
+						 ":prenom"=>$clientPrenom]);
 	// Ou
 	//$statement->bindParam(":nom", $_POST["clientNom"]);
 	//$statement->bindParam(":prenom", $_POST["clientPrenom"]);
